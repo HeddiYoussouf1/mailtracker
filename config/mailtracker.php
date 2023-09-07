652b8299ec8f18e7189975f07a0308c01f29440c
HeddiYoussouf1 <heddiyoussouf@gmail.com>
2023-09-07 02:07:12 +0100

    edit provider

--- config/mailtracker.php
+++ config/mailtracker.php
@@ -1,11 +1,12 @@
 <?php
-/**
- * Configuration for email tracking.
- *
- * - `image`: Specifies the path to the image that will be displayed at the footer of the email.
- *            This image serves the purpose of tracking the email. When the email is viewed, the
- *            image is loaded, indicating that the email has been opened.
- */
+
 return [
+ /*
+ | Configuration for email tracking.
+ |
+ | - `image`: Specifies the path to the image that will be displayed at the footer of the email.
+ |            This image serves the purpose of tracking the email. When the email is viewed, the
+ |            image is loaded, indicating that the email has been opened.
+ */
     'image' => 'assets/footer.png',
 ];
